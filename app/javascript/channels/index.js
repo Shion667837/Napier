13289822a161f1bc692b0935de28a9ef88d5420b
福田詩苑 <xionfortune@fukudautaennoMacBook-Pro-2.local>
2021-07-25 21:36:23 +0900

    index.jsの編集

--- app/javascript/channels/index.js
+++ app/javascript/channels/index.js
@@ -1,5 +1,7 @@
 // Load all the channels within this directory and all subdirectories.
 // Channel files must be named *_channel.js.
+import '../scss/style.scss';
+import $ from '../env/node_modules/jquery';
 
 const channels = require.context('.', true, /_channel\.js$/)
 channels.keys().forEach(channels)
