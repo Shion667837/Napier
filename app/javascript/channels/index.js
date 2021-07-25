50611cfa33323ad98036ffc0169db47466982d59
福田詩苑 <xionfortune@fukudautaennoMacBook-Pro-2.local>
2021-07-25 22:03:43 +0900

    gg

--- app/javascript/channels/index.js
+++ app/javascript/channels/index.js
@@ -1,7 +1,4 @@
 // Load all the channels within this directory and all subdirectories.
 // Channel files must be named *_channel.js.
-import '../scss/style.scss';
-import $ from '../env/node_modules/jquery';
-
 const channels = require.context('.', true, /_channel\.js$/)
 channels.keys().forEach(channels)
