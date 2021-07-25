c0938e7c7b6ba2e91be9950116b0444bfc3e3e78
福田詩苑 <xionfortune@fukudautaennoMacBook-Pro-2.local>
2021-07-26 03:18:32 +0900

    application.jsの編集

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -13,7 +13,6 @@ require("@rails/ujs").start()
 require("@rails/activestorage").start()
 require("channels")
 require('bootstrap/dist/js/bootstrap.min.js')
-import "../src/style.scss";
 require("jquery");
 require("bootstrap");
 
