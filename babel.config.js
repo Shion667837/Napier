23840466ac4ab6598a8b5ad53da2ad233f1b7cb3
福田詩苑 <xionfortune@fukudautaennoMacBook-Pro-2.local>
2021-07-26 00:53:01 +0900

    babel編集

--- babel.config.js
+++ babel.config.js
@@ -48,6 +48,10 @@ module.exports = function(api) {
         }
       ],
       [
+        "@babel/plugin-proposal-private-methods",
+        { "loose": true }
+      ],
+      [
         '@babel/plugin-proposal-object-rest-spread',
         {
           useBuiltIns: true
