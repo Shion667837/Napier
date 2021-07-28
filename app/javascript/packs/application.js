f757ec4604039fa9e071d78c38b76134f5713545
福田詩苑 <xionfortune@fukudautaennoMacBook-Pro-2.local>
2021-07-29 04:50:04 +0900

    一部修正

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -105,3 +105,9 @@ linkColor.forEach(l=> l.addEventListener('click', colorLink))
 
 const flashMsg = document.querySelector('.alert')
 
+//開閉ボタンを押した時には
+$(".open-btn").click(function () {
+  $(this).toggleClass('btnactive');//.open-btnは、クリックごとにbtnactiveクラスを付与＆除去。1回目のクリック時は付与
+  $("#search-wrap").toggleClass('panelactive');//#search-wrapへpanelactiveクラスを付与
+  $('#search-text').focus();//テキスト入力のinputにフォーカス
+});
