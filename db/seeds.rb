4a76ab380911236e835c4dc9b8eeca4a34848fc2
福田詩苑 <xionfortune@fukudautaennoMacBook-Pro-2.local>
2021-07-29 18:01:13 +0900

    初期データの作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,38 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Company.create!(
+  id: 1,
+  coop_name: 'TESLA',
+  coop_body: 'aaa',
+  coop_place: 'USA'
+)
+
+Company.create!(
+  id: 2,
+  coop_name: 'APPLE',
+  coop_body: 'aaa',
+  coop_place: 'USA'
+)
+
+Company.create!(
+  id: 3,
+  coop_name: 'GOOGLE',
+  coop_body: 'aaa',
+  coop_place: 'USA'
+)
+
+Company.create!(
+  id: 4,
+  coop_name: 'AMAZON',
+  coop_body: 'aaa',
+  coop_place: 'USA'
+)
+
+Company.create!(
+  id: 5,
+  coop_name: 'NETFLIX',
+  coop_body: 'aaa',
+  coop_place: 'USA'
+)
