6a3605d38e9c4b7b0ea84fa6de32a7f40f0d16b9
福田詩苑 <xionfortune@fukudautaennoMacBook-Pro-2.local>
2021-08-10 16:30:12 +0900

    レイアウト変更

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -119,3 +119,14 @@ $(document).ready(function () {
   });
 
 });
+
+$(function(){
+  $('.js-modal-open').on('click',function(){
+      $('.js-modal').fadeIn();
+      return false;
+  });
+  $('.js-modal-close').on('click',function(){
+      $('.js-modal').fadeOut();
+      return false;
+  });
+});
